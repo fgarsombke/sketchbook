9c8faa58e8659673033fe2f8ad0e58de6b62bc20
franz <franz@franz-ubuntu.(none)>
2013-01-07 21:03:56 -0700

    conversion to MQTT

--- H2LoController/H2LoController.ino
+++ H2LoController/H2LoController.ino
@@ -1,35 +1,33 @@
 /*
   H2LoController
- 
  created 04 Jan 2012
  by Franz Garsombke
- 
  */
 #include "Structures.h"
 #include <SPI.h>
 #include <Ethernet.h>
 #include <aJSON.h>
-#include <HttpClient.h>
-#include <b64.h>
 #include <MemoryFree.h>
 #include <PubSubClient.h>
 
-char server[] = "h2lo-api.herokuapp.com";
-// Path to download (this is the bit after the hostname in the URL
-// that you want to download
-const char path[] = "/service/device";
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+#define MQTT_SERVER "m2m.eclipse.org"
+#define M2MIO_USERNAME   ""
+#define M2MIO_PASSWORD   ""
+#define M2MIO_PASSWORD   ""
+#define M2MIO_DOMAIN     ""
+#define M2MIO_DEVICE_ID "arduino-h2lo-device"
+#define MQTT_KEEPALIVE 5
+char jsonDeviceString[] = "{\"id\":1,\"pin\":\"12345\"}";
+
+// Enter a MAC address for your controller below.Newer Ethernet shields have a MAC address printed on a sticker on the shield??
 byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-// Initialize the Ethernet client library
-// with the IP address and port of the server 
-// that you want to connect to (port 80 is default for HTTP):
-EthernetClient client;
-// Number of milliseconds to wait without receiving any data before we give up
-const int networkTimeout = 30*1000;
-// Number of milliseconds to wait if no data is available before trying again
-const int networkDelay = 1000;
+EthernetClient ethernetClient;
 String jsonResponse = "";
+char clientID[50];
+char topic[50];
+char msg[50];
+char message_buff[100];
+PubSubClient pubSubClient(MQTT_SERVER, 1883, callback, ethernetClient);
 
 void setup() {
   // start serial port:
@@ -46,108 +44,58 @@ void setup() {
   }
   // give the Ethernet shield a second to initialize:
   delay(1000);
-  Serial.println("connecting...");
+  Serial.println("connecting to ethernet...");
   // print the Ethernet board/shield's IP address:
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
-  printFreeMemory("After Setup");  
+  Serial.println("connected to Ethernet...");
+  printFreeMemory("After Setup"); 
+ 
+  String topicStr = "arduino/";
+  topicStr.concat("h2lo");
+  topicStr.toCharArray(topic, topicStr.length()+1);
+  Serial.print("topic:");
+  Serial.println(topic);    
+  Serial.println("connecting pub sub client");
+  pubSubClient.connect(M2MIO_DEVICE_ID);
+  pubSubClient.subscribe(topic);      
 }
 
 void loop() {
-  
-  int err =0;
-  
-  EthernetClient c;
-  // This HTTPClient library https://interactive-matter.eu/how-to/arduino-http-client-library 
-  // looks a little better (response is returned as a FILE* stream which aJSON can parse
-  // but the http.get command takes an physical IPAddress and not just a server name. WTF?
-  HttpClient http(c);
-  
-  err = http.get(server, path);
-  if (err == 0)
-  {
-    Serial.println("startedRequest ok");
-
-    err = http.responseStatusCode();
-    if (err >= 0)
-    {
-      Serial.print("Got status code: ");
-      Serial.println(err);
-
-      // Usually you'd check that the response code is 200 or a
-      // similar "success" code (200-299) before carrying on,
-      // but we'll print out whatever response we get
-
-      err = http.skipResponseHeaders();
-      if (err >= 0)
-      {
-        int bodyLen = http.contentLength();
-        Serial.print("Content length is: ");
-        Serial.println(bodyLen);
-        Serial.println();
-        Serial.println("Body returned follows:");
-
-        // Now we've got to the body, so we can print it out
-        unsigned long timeoutStart = millis();
-        char c;
-        // Whilst we haven't timed out & haven't reached the end of the body
-        while ( (http.connected() || http.available()) && ((millis() - timeoutStart) < networkTimeout) )
-        {
-            if (http.available())
-            {
-                c = http.read();
-                // Print out this character
-                Serial.print(c);
-                if (c != '[' && c != ']') {
-                  jsonResponse += c;              
-                }
-                bodyLen--;
-                // We read something, reset the timeout counter
-                timeoutStart = millis();
-            }
-            else {
-              // We haven't got any data, so let's pause to allow some to arrive
-              delay(networkDelay);
-            }
-        }
-      }
-      else
-      {
-        Serial.print("Failed to skip response headers: ");
-        Serial.println(err);
-      }
+   if (!pubSubClient.connected()) {
+      // clientID, username, MD5 encoded password
+      Serial.println("re-connecting pub sub client");
+      pubSubClient.connect(M2MIO_DEVICE_ID);
+      pubSubClient.subscribe(topic);      
     }
-    else
-    {    
-      Serial.print("Getting response failed: ");
-      Serial.println(err);
-    }
-  }
-  else
-  {
-    Serial.print("Connect failed: ");
-    Serial.println(err);
-  }
-  http.stop();
-  Serial.println();
-  Serial.print("jsonResponse:");
-  Serial.println(jsonResponse);
-  // We have the json response, let's start to parse it
-  jsonResponse.trim();
-  char* jsonString = (char*) malloc(sizeof(char)*(jsonResponse.length()+1));
-  jsonResponse.toCharArray(jsonString, jsonResponse.length() + 1);
-  printFreeMemory("Before parsing");  
-  Device device = parseDevice(jsonString);
-  if (true) {
-    Serial.print("Successfully Parsed pin:");
-    Serial.println(device.pin);
-    Serial.print("Successfully Parsed id:");
-    Serial.println(device.id);
-  } else {
-    Serial.println("There was some problem in parsing the JSON");
-  }
-  printFreeMemory("After parsing");  
-  // And just stop, now that we've tried a download
-  while(1);
+    // MQTT client loop processing
+    pubSubClient.loop();
+    pubSubClient.publish(topic, jsonDeviceString);
+    delay(5000);    
 }
 
+// handles message arrived on subscribed topic(s)
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.println("Message arrived:  topic: " + String(topic));
+  Serial.println("Length: " + String(length,DEC));
+  int i = 0;  
+  // create character buffer with ending null terminator (string)
+  for(i=0; i<length; i++) {
+    message_buff[i] = payload[i];
+  }
+  message_buff[i] = '\0';
+  String msgString = String(message_buff);
+  Serial.print("Payload:");
+  Serial.print(msgString);
+  Serial.println(":");
+  printFreeMemory("memory before parsing"); 
+  // Run out of memory after 4 parses!!! 
+  /*
+  Device device = parseDevice(message_buff);
+  Serial.print("Successfully Parsed pin:");
+  Serial.println(device.pin);
+  Serial.print("Successfully Parsed id:");
+  Serial.println(device.id);  
+  printFreeMemory("memory after parsing");  
+  */
+}
