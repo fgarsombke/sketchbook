d9bd7b848cc18288cb000bc37a3289fe77247303
franz <franz@franz-ubuntu.(none)>
2013-02-17 15:46:16 -0700

    fix wifi issues

--- H2LoController/H2LoController.ino
+++ H2LoController/H2LoController.ino
@@ -5,20 +5,22 @@
  */
 #include "Structures.h"
 #include <SPI.h>
-#include <Ethernet.h>
+//#include <Ethernet.h>
 #include <PubSubClient.h>
 #include <MemoryFree.h>
-#include <Event.h>
-#include <Timer.h>
 #include <aJSON.h>
 #include <QueueArray.h> // http://playground.arduino.cc/Code/QueueArray
+#include <WiFi.h>
+#include <WiFiClient.h>
 
 // Enter a MAC address for your controller below.Newer Ethernet shields have a MAC address printed on a sticker on the shield??
 byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-Timer timer;
-EthernetClient ethernetClient;
-EthernetServer server(80);  
-PubSubClient pubSubClient(MQTT_SERVER, 1883, callback, ethernetClient);
+//EthernetClient netClient;
+WiFiClient netClient;
+PubSubClient pubSubClient(MQTT_SERVER, 1883, callback, netClient);
+char ssid[] = "nintendo";          //  your network SSID (name) 
+char pass[] = "7ustESTenedr";   // your network password
+int status = WL_IDLE_STATUS;
 int zoneId;
 int currentCommand = CODE_CMD_NO_ACTION;
 long devicePIN = 0;
@@ -40,6 +42,8 @@ const int CR_START_TIME = 3;
 // create a stack of run schedules.
 QueueArray <Schedule> scheduleStack;
 char message_buff[200];
+unsigned long connect_alarm; // time of next connection check;
+boolean wifi_connected = false;
 
 void setup() {
   // start serial port:
@@ -48,62 +52,44 @@ void setup() {
   delay(1000);
   Serial.print(F("Free:"));
   Serial.println(getFreeMemory());  
-  IPAddress ip(192,168,1, 177);
-  Ethernet.begin(mac, ip);
+  initWiFi();
+  //IPAddress ip(192,168,1, 177);
+  //Ethernet.begin(mac, ip);
   // give the Ethernet shield a second to initialize:
   delay(2000);
   //Set relay pins to output
   for (int i = 0; i < zoneCount; i++){
     pinMode(zones[i], OUTPUT);
   }
-  Serial.println(F("mqtt initial connect"));
-  pubSubClient.connect(M2MIO_DEVICE_ID, M2MIO_USERNAME, M2MIO_PASSWORD);
-
+  // clear the zones
+  for (int i = 0; i < zoneCount; i++){
+    digitalWrite(zones[i], LOW);
+  }
   // load EEPROM information
   loadConfig();
-  
+  Serial.print(F("Device PIN:"));
+  Serial.println(WiFiConfig.devicePIN);  
   /* uncomment out to reset device PIN
   WiFiConfig.devicePIN = 0;
   saveConfig();
   */
-  // determine if we use our webserver for device intial configuration or not
-  if(WiFiConfig.devicePIN > 0) {
-    // set the global devicePIN
-    devicePIN = WiFiConfig.devicePIN;
-    subscribeToTopic();
-    // tell everyone we are alive!
-    publishHeartbeat();
-  } else {
-    // Start the weberver
-    //Serial.println("start web server");
-    server.begin();    
-  } 
-  // Run the 'callback' every 'period' milliseconds.
-  int publishHeartbeatEvent = timer.every(60000, publishHeartbeat);
+  // set the global devicePIN
+  devicePIN = WiFiConfig.devicePIN;
   Serial.print(F("after:"));
   Serial.println(freeRam());
+  // reset all connections
+  ensure_connected();
 }
 
 void loop() {
-  if (!pubSubClient.connected()) {
-    Serial.println(F("mqtt re-connecting"));
-    pubSubClient.connect(M2MIO_DEVICE_ID);
-    if(devicePIN > 0) {
-      subscribeToTopic(); 
-    }    
+  if (millis() > connect_alarm) {
+    ensure_connected(); 
   }
   // MQTT client loop processing
   pubSubClient.loop();
   delay(2000);
-  // determine if we use our webserver for device intial configuration or not
-  if(devicePIN == 0) {
-    // device has not been configured yet
-    consumeHttpRequest();
-  }
   // Must be called from 'loop'. This will service all the events associated with the timer. - http://playground.arduino.cc/Code/Timer
-  if(devicePIN > 0) {
-    timer.update();    
-  }
+  //timer.update();    
   // This action is received from callback. If there is a command value, execute on it
   if(currentCommand != CODE_CMD_NO_ACTION) {
     runCurrentCommand();
@@ -332,70 +318,57 @@ void subscribeToTopic() {
   pubSubClient.subscribe(subscribeTopic);  
 }
 
-void consumeHttpRequest() {
-  char email_buff[50];
-  // Easy test:
-  // curl -X POST -d "email=xxx@xxx.com" http://192.168.1.177
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println(F("new client"));
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // this is to retrieve POST data
-          int i = 0;  
-          // create character buffer with ending null terminator (string)          
-          while(client.available()) {
-            char t = client.read();
-            Serial.print(t);
-            email_buff[i] = t;
-            i++;
-          }
-          email_buff[i] = '\0';  
-          Serial.println();
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connnection: close");
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<form method=\"POST\">");
-          client.println("Email: <input type=\"text\" name=\"email\"><br>");
-          client.println("<input type=\"submit\" value=\"Submit\">");
-          client.println("</form>");
-          client.println("");       
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } 
-        else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
+/*
+ * method to make sure we have a connection to the Wifi and MQTT server
+ *
+ */
+void ensure_connected() {
+  static int failureCountMQTT = 0;
+  static int failureCountWiFi = 0;
+  Serial.println(F("Ensuring connection."));
+  connect_alarm = millis() + 30000;
+  if (!pubSubClient.connected()) {   
+    Serial.println(F("failureCountMQTT:"));
+    Serial.print(failureCountMQTT);
+    failureCountMQTT++;
+    if (!wifi_connected) {
+      Serial.println(F("failureCountWiFi:"));
+      Serial.print(failureCountWiFi);
+      failureCountWiFi++;
+      initWiFi();
     }
-    // give the web browser time to receive the data
-    delay(100);
-    // close the connection:
-    client.stop();
-    if(email_buff > 0) {
-      // subscribe the device
-      subscribe(email_buff);
-      // Now we clear our array
-      memset(email_buff, 0, sizeof(email_buff) );      
-    }  
-    Serial.println(F("client disonnected"));
+    mqtt_connect();
+  } else {
+    publishHeartbeat();
   }
+}
+
+void initWiFi() {
+  Serial.println(F("Attempting to connect to WPA network..."));
+  Serial.print(F("SSID:"));
+  Serial.println(ssid);
+  status = WiFi.begin(ssid, pass);
+  if ( status != WL_CONNECTED) { 
+    Serial.println(F("Couldn't get a wifi connection"));
+    wifi_connected = false;
+  } 
+  else {
+    Serial.print(F("Connected to wifi. My address:"));
+    IPAddress myAddress = WiFi.localIP();
+    Serial.println(myAddress);
+    wifi_connected = true;
+    delay(1000);
+  }
+}
+
+void mqtt_connect() {
+    Serial.println(F("Connecting to MQTT Broker..."));
+    if (pubSubClient.connect(M2MIO_DEVICE_ID, M2MIO_USERNAME, M2MIO_PASSWORD)) {
+      Serial.println(F("Connected to MQTT"));
+      subscribeToTopic();
+      // tell everyone we are alive!
+      publishHeartbeat();
+    } else {
+      Serial.println(F("Failed connecting to MQTT"));
+    }
 }
--- Mqtt/Mqtt.ino
+++ Mqtt/Mqtt.ino
@@ -17,25 +17,32 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
 
 // Update these with values suitable for your network.
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 
-#define MQTT_SERVER "192.168.1.13"
-#define M2MIO_USERNAME   ""
-#define M2MIO_PASSWORD   ""
+#define MQTT_SERVER "ec2-50-17-66-9.compute-1.amazonaws.com"
+#define M2MIO_USERNAME   "device"
+#define M2MIO_PASSWORD   "D2afrEXeSWumech4daSP"
 #define M2MIO_DOMAIN     ""
-#define M2MIO_DEVICE_ID "arduino-h2lo-device"
+#define M2MIO_DEVICE_ID "arduino-h2lo-deviceXXX"
+char ssid[] = "nintendo";          //  your network SSID (name) 
+char pass[] = "7ustESTenedr";   // your network password
+int status = WL_IDLE_STATUS;
 
 //char* jsonString = "hello world";
 //char* jsonString = "{\"deviceId\":2,\"zoneDurations\":[{\"zoneNumber\":3,\"duration\":5000},{\"zoneNumber\":4,\"duration\":10000}],\"hour\":0,\"minute\":0}";
-char* jsonString = "{\"deviceId\":2,\"zoneDurations\":[{\"zoneNumber\":3,\"duration\":5000}],\"hour\":0,\"minute\":0}";
+char* jsonString = "{\"d\":2,\"z\":1,\"s\":10}                    ";
+//char* jsonString = "test";
 
 // Callback function header
 void callback(char* topic, byte* payload, unsigned int length);
 
-EthernetClient ethClient;
-PubSubClient client(MQTT_SERVER, 1883, callback, ethClient);
+//EthernetClient netClient;
+WiFiClient netClient;
+PubSubClient client(MQTT_SERVER, 1883, callback, netClient);
 
 // Callback function
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -62,12 +69,18 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void setup() {
   // start serial port:
   Serial.begin(9600);
+  
+  //IPAddress ip(192,168,1, 177);
+  //Ethernet.begin(mac, ip);
+  
+  initWiFi();
   // give the ethernet module time to boot up:
   delay(1000);
-  IPAddress ip(192,168,1, 177);
-  Ethernet.begin(mac, ip);
-  if (client.connect("arduinoClient", "foobar", "foobarpassword")) {
+  if (client.connect(M2MIO_DEVICE_ID, M2MIO_USERNAME, M2MIO_PASSWORD)) {
+    Serial.print(F("Connected to MQTT"));
     client.subscribe("arduinoOutTopic");
+  } else {
+    Serial.print(F("Not connected to MQTT"));
   }
 }
 
@@ -75,9 +88,31 @@ void loop()
 {
   static int count = 0;
   client.loop();
-  client.publish("arduinoOutTopic", jsonString);
+  boolean publishSuccess = client.publish("arduinoOutTopic", jsonString);
+  Serial.print(F("Publish Success?"));
+  Serial.println(publishSuccess);
+  Serial.print(F("Still connected?"));
+  Serial.println(client.connected());
   delay(1000);
   count++;
   Serial.print(F("count:")); 
   Serial.println(count);  
+}
+
+void initWiFi() {
+  
+  Serial.println("Attempting to connect to WPA network...");
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+
+  status = WiFi.begin(ssid, pass);
+  if ( status != WL_CONNECTED) { 
+    Serial.println("Couldn't get a wifi connection");
+    while(true);
+  } 
+  else {
+    Serial.print("Connected to wifi. My address:");
+    IPAddress myAddress = WiFi.localIP();
+    Serial.println(myAddress);
+  }
 }
